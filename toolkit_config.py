ab4e4b48fc383a97bacbbab36e5513d5a6fb600b
zid5528530 Yachun Wang <164983274+zid5528530YachunWang@users.noreply.github.com>
2024-04-18 23:33:44 +0800

    Initial commit

---
+++ toolkit_config.py
@@ -0,0 +1,8 @@
+""" toolkit_config.py
+Project configuration file
+"""
+import os
+
+# Replace '???' with the path to your project directory
+PRJDIR = 'D:\\python-learn\\toolkit2'
+DATADIR = os.path.join(PRJDIR, 'data')
